b8c237c138b42c4c4b2de0e12e12e66f94edbdf4
EvgeniiPopovski <evgenii.almaz@gmail.com>
2020-04-06 16:42:49 +0300

    в Users Создана классовая компонента

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,39 +2,74 @@ import React from "react";
 import s from './Users.module.css'
 import * as axios from 'axios'
 
-const Users = (props) => {
 
-    if (props.users.length === 0 ) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then( (response) =>{
-            debugger;
-        props.setUsers(response.data.items)
-        } );
-        // props.setUsers(
-        //         [
-        //     {id: 1, avatarUrl: 'https://i.kym-cdn.com/photos/images/facebook/000/215/813/d57.png', isfollowing: false,fullname: 'Петров Иван',status: 'boss', location:{country: 'Belarus', city: 'Minsk'}},
-        //     {id: 2, avatarUrl: 'https://i.kym-cdn.com/photos/images/facebook/000/215/813/d57.png', isfollowing: true, fullname: 'Игонь Андреев',status: 'bigboss',location:{country: 'Russias', city: 'Orel'}},
-        //     {id: 3, avatarUrl: 'https://i.kym-cdn.com/photos/images/facebook/000/215/813/d57.png', isfollowing: false,fullname: 'Жанна Агузарова', status: 'very big boss',location:{country: 'Russias', city: 'Orel'}},
-        // ]
-        // )
-    };
+class Users extends React.Component {
+    constructor(props) {
+        super (props);
 
-    return (
-        props.users.map( u => (
-            <div key={u.id}>
-                <div >
-                    { u.photos.small === null ?  <img className={s.image} src={'https://i.kym-cdn.com/photos/images/facebook/000/215/813/d57.png'} /> : <img className={s.image} src={u.photos.small}/>}
+    }
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then( (response) => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+
+    render () {
+        return (
+            this.props.users.map(u => (
+                <div key={u.id} className={s.userWrapper}>
+                    <div className={s.photoAndButton}>
+                        <div>
+                            {u.photos.small === null ? <img className={s.image}
+                                                            src={'https://i.kym-cdn.com/photos/images/facebook/000/215/813/d57.png'}/> :
+                                <img className={s.image} src={u.photos.small}/>}
+                        </div>
+                        <div>
+                            {u.isfollowing ? <button className={s.followButton} onClick={() => (this.props.unfollow(u.id))}>Unfollow</button> :
+                                <button className={s.followButton} onClick={() => (this.props.follow(u.id))}>Follow</button>}
+                        </div>
+                    </div>
+                    <p>{u.name}</p>
+                    <p>{u.status}</p>
+                    <p className={s.location}>
+                        <span>{'u.location.country'}</span> <br/>
+                        <span>{'u.location.city'}</span>
+                    </p>
                 </div>
-                <div>
-                    {u.isfollowing ?  <button onClick={() => (props.unfollow(u.id))}>Unfollow</button> : <button onClick={() => (props.follow(u.id))}>Follow</button>}
-                </div>
-                <p>{u.name}</p>
-                <p>{u.status}</p>
-                <p>
-                    <span>{'u.location.country'}</span> <br/>
-                    <span>{'u.location.city'}</span>
-                </p>
-            </div>
-        ))
-    )
-};
+
+            ))
+        );
+    }
+}
+
+
+//
+// const Users = (props) => {
+//
+//     if (props.users.length === 0 ) {
+//         axios.get('https://social-network.samuraijs.com/api/1.0/users').then( (response) =>{
+//             debugger;
+//         props.setUsers(response.data.items)
+//         } );
+//     };
+//
+//     return (
+//         props.users.map( u => (
+//             <div key={u.id}>
+//                 <div >
+//                     { u.photos.small === null ?  <img className={s.image} src={'https://i.kym-cdn.com/photos/images/facebook/000/215/813/d57.png'} /> : <img className={s.image} src={u.photos.small}/>}
+//                 </div>
+//                 <div>
+//                     {u.isfollowing ?  <button onClick={() => (props.unfollow(u.id))}>Unfollow</button> : <button onClick={() => (props.follow(u.id))}>Follow</button>}
+//                 </div>
+//                 <p>{u.name}</p>
+//                 <p>{u.status}</p>
+//                 <p>
+//                     <span>{'u.location.country'}</span> <br/>
+//                     <span>{'u.location.city'}</span>
+//                 </p>
+//             </div>
+//         ))
+//     )
+// };
 export default Users;
